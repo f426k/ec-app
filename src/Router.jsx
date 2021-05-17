c1bae06aea2ee8f13138cca2b3c3d48eaf17daff
EC2 Default User <ec2-user@ip-172-31-45-248.ap-northeast-1.compute.internal>
2021-05-17 15:46:48 +0000

    added-productdetail

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Switch} from "react-router";
-import {ProductList, SignUp, Reset, SignIn, Home, ProductEdit} from "./templates";
+import {ProductDetail, ProductList, SignUp, Reset, SignIn, ProductEdit} from "./templates";
 import Auth from './Auth'
 
 const Router = () => {
@@ -11,6 +11,7 @@ const Router = () => {
             <Route exact path={"/signin/reset"} component={Reset} />
             <Auth>
                 <Route exact path={"(/)?"} component={ProductList} />
+                <Route exact path={"/product/:id"} component={ProductDetail} />
                 <Route path={"/product/edit(/:id)?"} component={ProductEdit} />
             </Auth>
         </Switch>
