f76dab90a5a2da0b6bb5500b9cd83fe95193b31c
EC2 Default User <ec2-user@ip-172-31-45-248.ap-northeast-1.compute.internal>
2021-05-22 15:00:07 +0000

    added-search

--- src/components/Header/ClosableDrawer.jsx
+++ src/components/Header/ClosableDrawer.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
@@ -16,6 +16,7 @@ import {TextInput} from '../UIkit/index';
 import {useDispatch} from "react-redux";
 import {push} from "connected-react-router";
 import {signOut} from '../../reducks/users/operations';
+import {db} from "../../firebase/index";
 
 
 
@@ -53,27 +54,48 @@ const ClosableDrawer = (props) => {
         props.onClose(event)
     };
     
+    const [filters, setFilters] = useState([
+        {func: selectMenu, label: "すべて", id: "all", value: "/"},
+        {func: selectMenu, label: "メンズ", id: "male", value: "/?gender=male"},
+        {func: selectMenu, label: "レディース", id: "female", value: "/?gender=female"},
+
+    ])
+    
     const menus = [
             {func: selectMenu, label: "商品登録", icon: <AddCircleIcon />, id: "register", value: "/product/edit"},
             {func: selectMenu, label: "注文履歴", icon: <HistoryIcon />, id: "history", value: "/order/history"},
             {func: selectMenu, label: "プロフィール", icon: <PersonIcon />, id: "profile", value: "/user/mypage"}
             
         ];
+        
+        useEffect(() => {
+            db.collection('categories')
+                .orderBy('order', 'asc')
+                .get()
+                .then(snapshots => {
+                    const list = []
+                    snapshots.forEach(snapshot => {
+                        const category = snapshot.data()
+                        list.push({func: selectMenu, label: category.name, id: category.id, value: `/?category=${category.id}`},)
+                    })
+                    setFilters(prevState => [...prevState, ...list])
+                })
+        }, []);
     
     return (
-        <nav className={classes.drawerPaper}>
+        <nav className={classes.drawer}>
             <Drawer
                 container={container}
                 variant="temporary"
                 anchor="right"
                 open={props.open}
-                onClose={(e) => props.onClose(e)}
+                onClose={(e) => props.onClose(e, false)}
                 classes={{paper: classes.drawerPaper}}
                 ModalProps={{keepMounted:true}}
             >
                 <div
-                    onClose={(e) => props.onClose(e)}
-                    onKeyDown={(e) => props.onClose(e)}
+                    onClose={(e) => props.onClose(e, false)}
+                    onKeyDown={(e) => props.onClose(e, false)}
                 >
                     <div className={classes.searchField}>
                         <TextInput
@@ -100,6 +122,18 @@ const ClosableDrawer = (props) => {
                             </ListItemIcon>
                             <ListItemText primary={"Logout"} />
                         </ListItem>
+                    </List>
+                    <Divider />
+                    <List>
+                        {filters.map(filter => (
+                            <ListItem 
+                                button 
+                                key={filter.id}
+                                onClick={(e) => filter.func(e, filter.value)}
+                            >
+                                <ListItemText primary={filter.label}/>
+                            </ListItem>
+                        ))}
                     </List>
                 </div>
             </Drawer>
